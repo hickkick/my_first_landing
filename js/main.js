b43428f1eb8e22f65046b06eee6b38c2db8ebdd5
Roman Oliinyk <inthewavves@gmail.com>
2019-12-19 21:56:38 +0200

    adding files

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('.gallery__inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+	});
+
+	$('.btn__menu').on('click', function(){
+        $('.menu').slideToggle();
+    });
+});
